60af0e5e1afdb1bede201ef262355123b072d9d7
dreamworldsn <107345428+gardenwow@users.noreply.github.com>
2022-10-22 15:25:02 +0400

    ''

---
+++ src/main/java/com/example/coursework2/number2/Number2Application.java
@@ -0,0 +1,13 @@
+package com.example.coursework2.number2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Number2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Number2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coursework2/number2/Number2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursework2.number2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Number2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
